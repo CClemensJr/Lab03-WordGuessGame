80e58e88096aeb02b9a3ad7dd80e27a8a319d9cb
CClemensJr <cclemensjr@gmail.com>
2019-01-11 01:01:52 -0800

    finished test methods

--- WordGuessTests/UnitTest1.cs
+++ WordGuessTests/UnitTest1.cs
@@ -7,13 +7,49 @@ namespace WordGuessTests
     public class UnitTest1
     {
         [Fact]
-        public void EditedWordsShouldBeUpdatedInFile()
+        public void ShouldBeAbleToEditWordsInFile()
         {
             string path = "dictionary.txt";
             string word = "yoke";
             string editedWord = "yokes";
 
-            Assert.Equal(editedWord, Program.UpdateFile(path, word));
+            Assert.Equal(editedWord, Program.EditFile(path, word));
+        }
+
+        [Fact]
+        public void ShouldBeAbleToAddWordsToFile()
+        {
+            string path = "dictionary.txt";
+            string newWord = "pokes";
+
+            Assert.Equal(newWord, Program.AddToFile(path, newWord));
+        }
+
+        [Fact]
+        public void ShouldBeAbleToReadWordsFromFile()
+        {
+            string path = "dictionary.txt";
+            string firstWordInFile = "yoke";
+
+            Assert.Equal(firstWordInFile, Program.ReadFile(path, firstWordInFile));
+        }
+
+        [Fact]
+        public void ShouldDetectIfGuessInWord()
+        {
+            string word = "yoke";
+            string guess = "o";
+
+            Assert.Equal("In word", Program.CheckGuess(word, guess));
+        }
+
+        [Fact]
+        public void ShouldDetectIfGuessNotInWord()
+        {
+            string word = "yoke";
+            string guess = "o";
+
+            Assert.Equal("Not in word", Program.CheckGuess(word, guess));
         }
     }
 }
