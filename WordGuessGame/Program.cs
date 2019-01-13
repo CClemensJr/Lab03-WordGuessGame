81624945eb560cb45b1b60f18a85a72ac8beda8b
CClemensJr <cclemensjr@gmail.com>
2019-01-12 22:11:00 -0800

    Created method to handle showing words

--- WordGuessGame/Program.cs
+++ WordGuessGame/Program.cs
@@ -9,12 +9,16 @@ namespace WordGuessGame
         {
             string path = "../../../../wordbank.txt";
 
-            ReadWordBank(path);
+            ShowWordBank(path);
 
             Console.WriteLine("\n\nPress any key to exit...");
             Console.ReadLine();
         }
 
+
+        /**
+         * FILE METHODS
+         **/
 
         /// <summary>
         /// ReadWordBank takes a path to text file and then opens (and closes) it using File.AllLines. The text read from File.AllLines is placed in an array and return to the method that call it.
@@ -36,6 +40,21 @@ namespace WordGuessGame
             }
 
             return null;
+        }
+
+
+
+        /**
+         * INTERFACE METHODS
+         **/
+        static void ShowWordBank(string path)
+        {
+            string[] words = ReadWordBank(path);
+
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
         }
     }
 
